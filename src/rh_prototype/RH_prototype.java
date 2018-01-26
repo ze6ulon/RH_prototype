f046a3b54275eee2f1eab913ec08f09d59557fc2
ze6ulon <ze6ulon@ze6ulon-V2>
2018-01-26 18:58:02 -0500

    First Commit

---
+++ src/rh_prototype/RH_prototype.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rh_prototype;
+
+/**
+ *
+ * @author ze6ulon
+ */
+public class RH_prototype {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Start RH Prototype Program");
+        System.out.println("Hello World");
+    }
+    
+}
