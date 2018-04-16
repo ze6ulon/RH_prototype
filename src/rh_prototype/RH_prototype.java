6c384ed7c17f3168e685748cb066dd246c94658f
ze6ulon <>
2018-04-16 19:37:42 -0300

    Merge origin/master

--- src/rh_prototype/RH_prototype.java
+++ src/rh_prototype/RH_prototype.java
@@ -5,6 +5,8 @@
  */
 package rh_prototype;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ze6ulon
@@ -16,8 +18,48 @@ public class RH_prototype {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Start RH Prototype Program");
-        System.out.println("Hello World");
+        Scanner scan = new Scanner(System.in);
+        String month, year;
+        int choice = 0;
+        
+        System.out.println("This software allows us to have monthly financial summury\n\n");
+        
+
+        System.out.println("Press <ENTREE> to continue...");
+        
+        System.out.print("Enter the budget month : ");
+        month = scan.next();
+        
+        System.out.print("Enter the budget year : ");
+        year = scan.next();
+        
+        System.out.println("-----------------------");
+        System.out.println("BUDGET - " + month + " " + year);
+        System.out.println("-----------------------");
+        System.out.println("1. Enter revenu");
+        System.out.println("2. Enter depenses");
+        System.out.println("3. Show bilan");
+        System.out.println("4. Exit");
+        System.out.println("Enter your choice: ");
+        choice = scan.nextInt();
+        switch(choice){
+            case 1:
+                System.out.println("Choice entered is 1");
+                break;
+            case 2:
+                System.out.println("Choice entered is 1");
+               break; 
+            case 3:
+                System.out.println("Choice entered is 1");
+                break;
+            case 4:
+                System.out.println("Choice entered is 1");
+                break;
+                    
+            default:
+                System.out.println("Choice entered is default");
+        }
+       
     }
     
 }
